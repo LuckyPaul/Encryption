f161e6050fc7e464370a14a3c7e22e99cba483ab
WangShenXiao <771048130@qq.com>
2018-09-12 17:39:37 +0800

    Iint and ADD RSA、 DES、AES、3DES

---
+++ Encryption/Encryption/Singleton.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Encryption
+{
+    public class Singleton<T> where T : new()
+    {
+        private static object _Mutex = new object();
+        private static T instance = default(T);
+        public static T Instance
+        {
+            get
+            {
+                if (instance == null)
+                {
+                    lock (_Mutex)
+                    {
+                        if (instance == null)
+                        {
+                            instance = new T();
+                        }
+                    }
+
+                }
+
+                return instance;
+            }
+
+        }
+
+        protected Singleton()
+        {
+            Init();
+            if (instance != null)
+            {
+                // throw new SingletonException(GetType() + "This Singleton is already exist ! Please not new again !!!");
+            }
+        }
+
+        protected virtual void Init() { }
+    }
+}
